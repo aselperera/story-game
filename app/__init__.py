e1c812ee22d46f824a12b465681855aa902f5b70
aselperera <aselperera@hotmail.com>
2024-10-02 16:24:50 +0100

    Flask environment variables setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
     # Basic route for testing
     @app.route('/')
